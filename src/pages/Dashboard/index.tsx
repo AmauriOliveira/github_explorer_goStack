47b986d79859ba327cce5b7a5589fc59daa098f3
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-09-09 21:40:12 -0300

    inicio da integração com api do github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/githubLogo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FunctionComponent = () => {
+  const [repositories, setRepositories] = useState([]);
+
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
@@ -15,23 +19,6 @@ const Dashboard: React.FunctionComponent = () => {
         <button type="submit">Pesquisar</button>
       </Form>
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/22779227?s=460&u=bd374c5009fd9a146fc8a10fcd63020ca1ea34bb&v=4"
-            alt="Amauri Oliveira"
-          />
-          <div>
-            <strong>AmauriOliveira/amaurioliveira</strong>
-            <p>
-              Lorem ipsum Proin suscipit luctus orci placerat fringilla. Donec
-              hendrerit laoreet risus eget adipiscing. Suspendisse in urna
-              ligula, a volutpat mauris. Sed enim mi, adipiscing eu pulvinar
-              vel, sodales vitae dui.
-            </p>
-          </div>
-          <FiChevronRight size={30} />
-        </a>
-        {/* //// */}
         <a href="teste">
           <img
             src="https://avatars2.githubusercontent.com/u/22779227?s=460&u=bd374c5009fd9a146fc8a10fcd63020ca1ea34bb&v=4"
