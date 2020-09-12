263348dc6d0fd20fce54557c58bb03b908d49880
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-09-12 08:46:12 -0300

    integração com api git completa

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,6 +7,7 @@ import api from '../../services/api';
 import logoImg from '../../assets/githubLogo.svg';
 
 import { Title, Form, Repositories } from './styles';
+import Repository from '../Repository';
 
 interface Repository {
   full_name: string;
@@ -26,7 +27,10 @@ const Dashboard: React.FunctionComponent = () => {
   ): Promise<void> {
     event.preventDefault(); // suprimir comportamento padrão do form
 
-    const response = await api.get(`repos/${newRepo}`);
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    setNewRepo(''); // limpar input
+
     const repository = response.data;
 
     setRepositories([...repositories, repository]);
@@ -44,22 +48,19 @@ const Dashboard: React.FunctionComponent = () => {
         <button type="submit">Pesquisar</button>
       </Form>
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/22779227?s=460&u=bd374c5009fd9a146fc8a10fcd63020ca1ea34bb&v=4"
-            alt="Amauri Oliveira"
-          />
-          <div>
-            <strong>AmauriOliveira/amaurioliveira</strong>
-            <p>
-              Lorem ipsum Proin suscipit luctus orci placerat fringilla. Donec
-              hendrerit laoreet risus eget adipiscing. Suspendisse in urna
-              ligula, a volutpat mauris. Sed enim mi, adipiscing eu pulvinar
-              vel, sodales vitae dui.
-            </p>
-          </div>
-          <FiChevronRight size={30} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={30} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
