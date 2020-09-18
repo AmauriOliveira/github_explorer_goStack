8ddf153e258b90396c75f4e19050d6150a95bed8
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-09-17 22:19:46 -0300

    dados estaticos para desenvolver a repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft } from 'react-icons/fi';
 
-import { Header } from './styles';
+import { Header, RepositoryInfo } from './styles';
 import logoImg from '../../assets/githubLogo.svg';
 
 interface RepositoryParams {
@@ -12,13 +12,28 @@ interface RepositoryParams {
 const Repository: React.FunctionComponent = () => {
   // const { params } = useRouteMatch<RepositoryParams>();
   return (
-    <Header>
-      <img src={logoImg} alt="GitHub Explorer" />
-      <Link to="/dashboard">
-        <FiChevronLeft size={16} />
-        Voltar
-      </Link>
-    </Header>
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/22779227?s=400&u=bd374c5009fd9a146fc8a10fcd63020ca1ea34bb&v=4"
+            alt="bla bla"
+          />
+          <div>
+            <strong>AmauriOliveira/AmauriOliveira</strong>
+            <p>Descricão</p>
+          </div>
+        </header>
+        <ul />
+      </RepositoryInfo>
+    </>
   );
 };
 
